91541d6f652a69434b28ae97896afac4150608a3
Ahmad Saifuddin <saifuddin.fakhri@gmail.com>
2015-10-02 10:20:33 -0500

    ignore output.txt

--- main.rb
+++ main.rb
@@ -1,14 +1,36 @@
+require 'ruby-progressbar'
+
+progressbar = ProgressBar.create
+
 paragraphs = File.readlines("article.txt").map(&:chomp)
 cities = File.readlines('places.txt').map(&:chomp)
+output = File.open( "output.txt","w" )
+count = 0
+chunks = paragraphs.count/100
+
+p "Starting.."
+
 paragraphs.each do |paragraph|
-  p "========"
-  p paragraph
-  p "--"
+  if (count > chunks)
+    progressbar.increment
+    count = 0
+  end
+  output << "========"
+  output << "\n"
+  output << paragraph
+  output << "\n"
+  output << "--"
+  output << "\n"
   contains = []
-  cities.each do |city|
-    if paragraph.include? city
-      contains << city
+  cities.each_with_index do |city,index|
+    if paragraph[/\b#{city}\b/]
+      contains << "#{city} (#{index})" unless city.empty?
     end
   end
-  p "Found these cities: " + contains.uniq.reject {|c| c.empty?}.join(", ")
+  # output << "Found these cities: " + contains.uniq.reject {|c| c.empty?}.join(", ")
+  output << "Found these cities: " + contains.join(", ")
+  output << "\n"
+  count += 1
 end
+p "Done!"
+output.close
